70ef84a40882f968011de9e92edbde29d44d57d9
alain-x <alainvalentin04@gmail.com>
2026-03-18 12:40:41 +0200

    initial commit

--- src/main/java/com/digital_timetable/security/WebSocketConfig.java
+++ src/main/java/com/digital_timetable/security/WebSocketConfig.java
@@ -1,17 +1,24 @@
 package com.digital_timetable.security;
 
+import com.digital_timetable.controller.WebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
+
+    @Autowired
+    private WebSocketHandler webSocketHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -36,9 +43,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // Raw WebSocket endpoint for simple JSON messaging (non-STOMP)
+        registry.addHandler(webSocketHandler, "/ws/raw")
+                .setAllowedOriginPatterns("*");
+    }
+
+    @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
         registration.setMessageSizeLimit(64 * 1024) // 64KB
                    .setSendBufferSizeLimit(512 * 1024) // 512KB
                    .setSendTimeLimit(20000); // 20 seconds
     }
-} 
+}
